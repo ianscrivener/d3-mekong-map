2306c2bc9aaa67bb476dc13aa63af7c596dc3b4c
Ian Scrivener <github@zilogy.asia>
2014-01-17 14:34:26 +1100

    Update spatial-convert.sh

--- spatial-convert.sh
+++ spatial-convert.sh
@@ -1,3 +1,14 @@
+mkdir downloads
+cd downloads
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_subunits.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places.zip
+
+unzip ne_10m_populated_places.zip 
+unzip ne_10m_admin_0_map_subunits.zip
+
+cd ../
+
 rm *.json
 
 ogr2ogr \
